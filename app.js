1d1d14ad523d810dbd0a71037858f23e2e022023
Ayal Ganem <Ayal.Ganem@kryonsystems.com>
2022-05-08 20:57:13 +0300

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/users', usersRouter);
+app.use('/users', usersRouter); # ewrjhewhrej
 app.use('/*', usersRouter);
 
 module.exports = app;
